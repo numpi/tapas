75d61f2e8f7b54c8b8cd8b49990b6d957a64e4e7
Giulio Masetti <giulio.masetti@isti.cnr.it>
2021-11-10 16:40:09 +0100

    Work on tutorial

--- examples/run_tutorial_large.m
+++ examples/run_tutorial_large.m
@@ -1,15 +1,17 @@
 minm = 5;
 maxm = 15;
-results = zeros(maxm-minm,3);
-for m = minm : maxm
-    [MTTF, CMTTF, CMTTB] = tutorial_large(m);
-    results(m-minm+1,1) = MTTF;
-    results(m-minm+1,2) = CMTTF;
-    results(m-minm+1,3) = CMTTB;
-end
+% results = zeros(maxm-minm,3);
+% for m = minm : maxm
+%     [MTTF, CMTTF, CMTTB] = tutorial_large(m);
+%     results(m-minm+1,1) = MTTF;
+%     results(m-minm+1,2) = CMTTF;
+%     results(m-minm+1,3) = CMTTB;
+% end
 
-slg=semilogy(minm:maxm,results(:,1),'--',minm:maxm,results(:,2),minm:maxm,results(:,3),'o');
-xlabel('m (number of copies of A1 in Figure 1)');
+slg=semilogy(minm+2:maxm+2,results(:,1),'--',minm+2:maxm+2,results(:,2),minm+2:maxm+2,results(:,3),'o');
+xticks(minm+2:maxm+2);
+axis([minm+2,maxm+2,1.0e+1,1.5e+5])
+xlabel('n (number of submodels)');
 ylabel('hours');
 legend('MTTF','CMTTF','CMTTB','location','northwest');
 slg(1).Color = [0 0 0];
